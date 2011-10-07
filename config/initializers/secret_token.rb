b2e5cec5e96bf15faa49a5dbdf9287e34c26e943
Wm Schwan <bschwan1@lycos.com>
2011-10-07 12:42:38 -0600

    Initial Checkin - Baseline of code developed for the ROR - Getting Started tutorial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoshxDemo::Application.config.secret_token = '063d57d31666bec1790eb38878606d0bbde35f5596d8d3508115863775b8147a128458e72deac85cb946f797a072f89c2e2a9ec95efe403aac438d2fe6f6b30a'
